7a8477976541e3a308dc4b82da9d94217418fd71
Dimmi <pechunka11@gmail.com>
2018-09-04 13:26:37 +0200

    sfdgsdf

--- app/js/common.js
+++ app/js/common.js
@@ -17,9 +17,9 @@ $(function() {
     $(this).siblings('.dropdown').children('ul').slideUp();
     if(window.innerWidth > 768){
       if($('.nav_top li').hasClass('active_mnu')){
-        $('header').css('box-shadow', '0 4px 0px 5000px rgba(0,0,0,.2)')
+        $('header').css('box-shadow', '0 4px 0px 5000px rgba(0,0,0,.6)')
       }else{
-        $('header').css('box-shadow', '0 4px 45px 0 rgba(0,0,0,.2)')
+        $('header').css('box-shadow', '0 4px 5000px 0 rgba(0,0,0,0)')
       }
     }
   })
@@ -28,21 +28,22 @@ $(function() {
 		if($('.nav_top li').hasClass('active_mnu')){
 			$('.dropdown').siblings('.dropdown').removeClass('active_mnu')
 	    $('.dropdown').siblings('.dropdown').children('ul').slideUp();
-			$('header').css('box-shadow', '0 4px 45px 0 rgba(0,0,0,.2)')
+			$('header').css('box-shadow', '0 4px 5000px 0 rgba(0,0,0,0)')
 		}
 	})
 
   $('.search > img').click(function() {
     $(this).prev().css('width', '100%');
+    $('#input_search').focus();
     if(window.innerWidth > 768){
-      $('header').css('box-shadow', '0 4px 0px 5000px rgba(0,0,0,.2)')
+      $('header').css('box-shadow', '0 4px 0px 5000px rgba(0,0,0,.6)')
     }
   })
 
   $('.search > form > img').click(function() {
     $('.search > form').css('width', '0')
     if(window.innerWidth > 768){
-      $('header').css('box-shadow', '0 4px 45px 0 rgba(0,0,0,.2)')
+      $('header').css('box-shadow', '0 4px 5000px 0 rgba(0,0,0,0)')
     }
   })
 
