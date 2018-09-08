7b2b3382d5d25ada8ae1ac570a8eccda812a409a
Dimmi <pechunka11@gmail.com>
2018-09-08 13:59:46 +0200

    sdgfd

--- app/js/common.js
+++ app/js/common.js
@@ -28,7 +28,7 @@ $(function() {
       if($('.nav_top li').hasClass('active_mnu')){
         $('header').css('box-shadow', '0 4px 0px 5000px rgba(0,0,0,.6)')
       }else{
-        $('header').css('box-shadow', '0 4px 5000px 0 rgba(0,0,0,0)')
+        $('header').css('box-shadow', '0 4px 45px 0 rgba(0,0,0,.2)')
       }
     }
   })
@@ -37,7 +37,7 @@ $(function() {
 		if($('.nav_top li').hasClass('active_mnu')){
 			$('.dropdown').siblings('.dropdown').removeClass('active_mnu')
 	    $('.dropdown').siblings('.dropdown').children('ul').slideUp();
-			$('header').css('box-shadow', '0 4px 5000px 0 rgba(0,0,0,0)')
+			$('header').css('box-shadow', '0 4px 45px 0 rgba(0,0,0,.2)')
 		}
 	})
 
@@ -52,7 +52,7 @@ $(function() {
   $('.search > form > span').click(function() {
     $('.search > form').css('width', '0')
     if(window.innerWidth > 768){
-      $('header').css('box-shadow', '0 4px 5000px 0 rgba(0,0,0,0)')
+      $('header').css('box-shadow', '0 4px 45px 0 rgba(0,0,0,.2)')
     }
   })
 
